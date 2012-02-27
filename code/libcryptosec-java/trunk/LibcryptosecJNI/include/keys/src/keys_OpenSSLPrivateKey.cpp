ae26592879f40c1021228d24c81e4b93a6e84306
lucasgm@inf.ufsc.br <lucasgm@inf.ufsc.br@814833e4-271b-0410-8600-8914c28c18a0>
2012-02-27 15:13:11 +0000

    Adição dos arquivos cpp para Public e PrivateKey

---
+++ code/libcryptosec-java/trunk/LibcryptosecJNI/include/keys/src/keys_OpenSSLPrivateKey.cpp
@@ -0,0 +1,13 @@
+
+
+void Java_keys_OpenSSLPrivateKey__1delete(JNIEnv *jNIEnv, jobject jobject)
+{
+}
+
+/*
+ * keys_OpenSSLPrivateKey.cpp
+ *
+ *  Created on: Feb 27, 2012
+ *      Author: lucasgm
+ */
+
