94e86d65aefb43cfadeadedfd22de53e62d0d512
vinicius.ss <schweitzer.vinicius@gmail.com>
2018-06-05 19:28:57 -0300

    Correção de inconsistências entre long e jlong
    - Alterações no método getInstance da classe Utils, alterando o método GetFieldID para aceitar Long
    - Alterações no método getObjectVector da classe Utils, alterando o tipo da var references de long para jlong
    - Alterações no OpensslRDNSequence, retornando para a versão original

--- lib/x509/OpensslRdnSequence.cpp
+++ lib/x509/OpensslRdnSequence.cpp
@@ -3,39 +3,33 @@
 #include <libcryptosec/certificate/RDNSequence.h>
 #include "util/Util.h"
 
-RDNSequence* sequence;
 
 jlong Java_br_ufsc_labsec_libcryptosec_x509_OpensslRdnSequence__1init(JNIEnv* env, jobject obj)
 {
-	sequence = new RDNSequence();
-	// return jlong(new RDNSequence());
-	return jlong(sequence);
+	return jlong(new RDNSequence());
 }
 
 void Java_br_ufsc_labsec_libcryptosec_x509_OpensslRdnSequence__1addEntry__Ljava_lang_String_2Ljava_lang_String_2(JNIEnv* env, jobject obj, jstring _entryTypeString, jstring _entryValue)
 {
-	// RDNSequence* sequence = Util::getInstance<RDNSequence*>(env, obj);
-	RDNSequence* new_sequence = sequence;
+	RDNSequence* sequence = Util::getInstance<RDNSequence*>(env, obj);
 
 	std::string entryTypeString = Util::jstringToString(env, _entryTypeString);
-	std::string entryValue = Util::jstringToString(env, _entryValue);
 	RDNSequence::EntryType entryType = Util::stringToRDNSequenceEntryType(entryTypeString);
-	new_sequence->addEntry(entryType, entryValue);
+	std::string entryValue = Util::jstringToString(env, _entryValue);
+	sequence->addEntry(entryType, entryValue);
 }
 
 void Java_br_ufsc_labsec_libcryptosec_x509_OpensslRdnSequence__1addEntry__Ljava_lang_String_2_3Ljava_lang_String_2(JNIEnv* env, jobject obj, jstring _entryTypeString, jobjectArray _entryValues)
 {
-	// RDNSequence* sequence = Util::getInstance<RDNSequence*>(env, obj);
-	RDNSequence* new_sequence = sequence;
+	RDNSequence* sequence = Util::getInstance<RDNSequence*>(env, obj);
 
 	std::string entryTypeString = Util::jstringToString(env, _entryTypeString);
 	RDNSequence::EntryType entryType = Util::stringToRDNSequenceEntryType(entryTypeString);
 	std::vector<std::string> entryValues = Util::jstringArrayTostringVector(env, _entryValues);
-	new_sequence->addEntry(entryType, entryValues);
+	sequence->addEntry(entryType, entryValues);
 }
 
 void Java_br_ufsc_labsec_libcryptosec_x509_OpensslRdnSequence__1delete(JNIEnv* env, jobject obj)
 {
-	// Util::deleteInstance<RDNSequence*>(env, obj);
-	delete sequence;
+	Util::deleteInstance<RDNSequence*>(env, obj);
 }
